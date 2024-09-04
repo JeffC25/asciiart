3229dfc18701438ca2363da18153b153db940093
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-09-03 20:49:29 -0400

    set up converter struct

--- converter/converter.go
+++ converter/converter.go
@@ -1,1 +1,31 @@
 package converter
+
+import "image"
+
+type Converter struct {
+	img   image.Image
+	scale int
+}
+
+type Options struct {
+	scale int
+}
+
+func NewConverter(img image.Image, opts ...func(*Converter)) *Converter {
+	c := &Converter{
+		img:   img,
+		scale: 8,
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
+}
+
+func WithScale(scale int) func(*Converter) {
+	return func(c *Converter) {
+		c.scale = scale
+	}
+}
